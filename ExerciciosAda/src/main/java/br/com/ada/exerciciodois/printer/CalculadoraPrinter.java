7ba103b256d2f4506f3d420d411934c9f24e2fe4
PedroQueiroz1 <pedroqueirozdev@outlook.com>
2023-08-27 15:43:40 -0300

    Reorganiza o codigo

--- ExerciciosAda/src/main/java/br/com/ada/exerciciodois/printer/CalculadoraPrinter.java
+++
@@ -1,9 +0,0 @@
-package br.com.ada.exerciciodois.printer;
-
-public class CalculadoraPrinter {
-
-    public void resultadoDaOperacao(Double resultado) {
-        System.out.println("Resultado: " + resultado);
-    }
-
-}
